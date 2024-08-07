8d099165fcfadc9bfba2a15a0b6afa74ae786999
KomfortKimko <komfortkimk@gmail.com>
2024-08-07 20:41:38 +0300

    React-router-nested-routes

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
 function UserCard({user}) {
-    return (
-      <article>
-          <h2>{user.name}</h2>
-          <Link to={`/profile/${user.id}`}>View profile</Link>
-      </article>
-    );
-  };
+  const users = useOutletContext();
+  console.log(users);
+  return (
+    <article>
+        <h2>{user.name}</h2>
+        <Link to={`/profile/${user.id}`}>View profile</Link>
+    </article>
+  );
+};
   
-  export default UserCard;
+export default UserCard;
